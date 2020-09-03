3a41ecac974271551fcb780b6c7534695279dd56
Kyle Xyian Dilbeck <GRANDMASTAH_XYIAN@users.noreply.github.com>
2020-09-03 13:49:03 -0700

    android test

--- app/src/main/java/com/kxdilbeck/gradeapp/MainActivity.java
+++ app/src/main/java/com/kxdilbeck/gradeapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//kk
+//test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
