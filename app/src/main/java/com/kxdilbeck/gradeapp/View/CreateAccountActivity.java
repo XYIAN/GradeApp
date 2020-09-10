22522f87b4652ef78a745bf7090644774c4b3dfa
Jason Buckley <jbuckley@csumb.edu>
2020-09-10 15:29:21 -0700

    adding start to create account activity

--- app/src/main/java/com/kxdilbeck/gradeapp/View/CreateAccountActivity.java
+++ app/src/main/java/com/kxdilbeck/gradeapp/View/CreateAccountActivity.java
@@ -1,4 +1,21 @@
 package com.kxdilbeck.gradeapp.View;
 
-public class CreateAccountActivity {
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kxdilbeck.gradeapp.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+    public static Intent getIntent(Context context){
+        return new Intent(context.getApplicationContext(), CreateAccountActivity.class);
+    }
 }
