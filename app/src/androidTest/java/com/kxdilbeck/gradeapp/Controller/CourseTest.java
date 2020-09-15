5ccbe311576e8e4caeed8ff7a8b0a4072ab55205
Kyle Xyian Dilbeck <GRANDMASTAH_XYIAN@users.noreply.github.com>
2020-09-15 15:49:01 -0700

    CoursePage test update(unfinished)

--- app/src/androidTest/java/com/kxdilbeck/gradeapp/Controller/CourseTest.java
+++ app/src/androidTest/java/com/kxdilbeck/gradeapp/Controller/CourseTest.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import androidx.room.Room;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.kxdilbeck.gradeapp.Model.Course;
 import com.kxdilbeck.gradeapp.Model.Database.AppDatabase;
 import com.kxdilbeck.gradeapp.Model.Database.UserDAO;
 import com.kxdilbeck.gradeapp.Model.User;
@@ -19,27 +20,29 @@ import static org.junit.Assert.*;
 public class CourseTest {
 
     //private UserDAO userDAO;
-    private CourseTest courseTest;
+    private Course course;
 
     @Before
     public void setUp() throws Exception {
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         //userDAO = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).allowMainThreadQueries().build().getUserDAO();
 
-        courseTest = new CourseTest();
+        //courseTest = new CourseTest();
     }
 
     @After
     public void tearDown() throws Exception {
         //userDAO = null;
-        courseTest = null;
+        course = null;
     }
 
     @Test
     public void checkCourse() {
         //main test
         CourseTest courseTest = new CourseTest();
+        assertTrue(course.courseTest());
     }
+
 }
 
 
--- app/src/main/java/com/kxdilbeck/gradeapp/Controller/CourseController.java
+++ app/src/main/java/com/kxdilbeck/gradeapp/Controller/CourseController.java
@@ -3,6 +3,7 @@ package com.kxdilbeck.gradeapp.Controller;
 import android.content.Context;
 import android.util.Log;
 
+import androidx.core.widget.TextViewCompat;
 import androidx.room.Room;
 
 import com.kxdilbeck.gradeapp.Model.Course;
@@ -13,13 +14,10 @@ import com.kxdilbeck.gradeapp.Model.User;
 
 public class CourseController {
     private UserDAO mUserDAO;
-    private User currentUser;
+    private Course currentCourse;
 
     public CourseController(Context context){
-        mUserDAO = Room.databaseBuilder(context, AppDatabase.class, AppDatabase.DBNAME)
-                .allowMainThreadQueries()
-                .build().getUserDAO();
-
+        mUserDAO = Room.databaseBuilder(context, AppDatabase.class, AppDatabase.DBNAME).allowMainThreadQueries().build().getUserDAO();
         //creates user in db if there is not one.  @TODO create separate class for pre population of data
         if(!context.getDatabasePath(AppDatabase.DBNAME).exists()){
             CourseDAO courseDAO;
@@ -28,17 +26,26 @@ public class CourseController {
                     .allowMainThreadQueries()
                     .build().getCourseDAO();
 
-            //if(courseDAO.getAllCourses().size() == 0){
-            courseDAO.insert(new Course("Dr. teacher", "BIO", "title here", "12/12/12", "12/13/12"));
-            courseDAO.insert(new Course("Dr. guy", "SCIENCE", "title here", "12/12/12", "12/13/12"));
-            courseDAO.insert(new Course("Dr. professor", "HISTORY", "title here", "12/12/12", "12/13/12"));
-            courseDAO.insert(new Course("Dr. pro", "ANATOMY", "title here", "12/12/15", "12/13/12"));
-            courseDAO.insert(new Course("Dr. it", "CS", "title here", "12/12/15", "12/13/12"));
-            courseDAO.insert(new Course("Dr. llama", "CS", "title here", "12/12/16", "12/13/12"));
-            courseDAO.insert(new Course("Dr. Evil", "BIO", "title here", "12/12/15", "12/13/12"));
-            //}
         }
     }//end course controller
+
+    public CourseController(){};
+
+    public boolean checkCourseID(int id){
+        //if(id == )
+        return true;
+    }
+    public boolean checkInstructor(String instructor){
+        if(currentCourse.getInstructor() == instructor) {
+            return true;
+        }
+        return false;
+    }
+    public boolean checkCourseStartDate(String date){
+        if(currentCourse.getStartDate() == date)
+        return true;
+    }
+
 
 
 
