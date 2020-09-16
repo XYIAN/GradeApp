e8bd3738ee8f1d5d0e5b9d3a168e368943dcc7b5
Jason Buckley <jbuckley@csumb.edu>
2020-09-15 23:49:31 -0700

    implemented tests for the grading methods

--- app/src/androidTest/java/com/kxdilbeck/gradeapp/Controller/GradeControllerTest.java
+++ app/src/androidTest/java/com/kxdilbeck/gradeapp/Controller/GradeControllerTest.java
@@ -12,6 +12,7 @@ import com.kxdilbeck.gradeapp.Model.Database.AppDatabase;
 import com.kxdilbeck.gradeapp.Model.Database.AssignmentDAO;
 import com.kxdilbeck.gradeapp.Model.Database.CourseDAO;
 import com.kxdilbeck.gradeapp.Model.Database.GradeCategoryDAO;
+import com.kxdilbeck.gradeapp.Model.Database.GradeDAO;
 import com.kxdilbeck.gradeapp.Model.Database.UserDAO;
 import com.kxdilbeck.gradeapp.Model.Grade;
 import com.kxdilbeck.gradeapp.Model.GradeCategory;
@@ -27,16 +28,18 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class GradeControllerTest {
+    private AppDatabase db;
     private GradeController gradeController;
     private UserDAO userDAO;
     private CourseDAO courseDAO;
     private AssignmentDAO assignmentDAO;
     private GradeCategoryDAO gradeCategoryDAO;
+    private GradeDAO gradeDAO;
 
     @Before
     public void setUp() throws Exception {
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        AppDatabase db =  Room.inMemoryDatabaseBuilder(context, AppDatabase.class)
+        db =  Room.inMemoryDatabaseBuilder(context, AppDatabase.class)
                 .allowMainThreadQueries()
                 .build();
         Prepopulate.prepopulate(db);
@@ -44,17 +47,20 @@ public class GradeControllerTest {
         courseDAO = db.getCourseDAO();
         gradeCategoryDAO = db.getGradeCategoryDAO();
         assignmentDAO = db.getAssignmentDAO();
+        gradeDAO = db.getGradeDAO();
 
         gradeController = new GradeController(db);
     }
 
     @After
     public void tearDown() throws Exception {
+        db.close();
         gradeController = null;
         userDAO = null;
         courseDAO = null;
         gradeCategoryDAO = null;
         assignmentDAO = null;
+        gradeDAO = null;
     }
 
     @Test
@@ -63,31 +69,58 @@ public class GradeControllerTest {
         List<Course> courses = courseDAO.getAllEnrolledCourses(users.get(0).getUserId());
 
         String resultant[] = gradeController.getCourseGrade(users.get(0).getUserId(), courses.get(0).getCourseId());
-        String resultant2[] = gradeController.getCourseGrade(users.get(1).getUserId(), courses.get(1).getCourseId());
-
-        List<Assignment> assignments = assignmentDAO.getAssignmentsByCategory(courses.get(0).getCourseId(), users.get(0).getUserId(), 1);
-
-        Log.i("DOESITWORK", assignments + "");
-
-        Log.i("DOESITWORK", resultant2[0] + ": " + resultant2[1] + " " + courses.get(1).getCourseId() + " " + courses.get(1).getTitle());
+        String resultant2[] = gradeController.getCourseGrade(users.get(0).getUserId(), courses.get(1).getCourseId());
+        String resultant3[] = gradeController.getCourseGrade(users.get(0).getUserId(), courses.get(2).getCourseId());
 
         assertEquals("B", resultant[0]);
         assertEquals(86.667, Double.parseDouble(resultant[1]), 0.01);
+        assertEquals("B", resultant2[0]);
+        assertEquals(82.444, Double.parseDouble(resultant2[1]), 0.01);
+        assertEquals(" ", resultant3[0]);
+        assertEquals(" ", resultant3[1]);
     }
 
     @Test
     public void getCourseGradeCategories() {
+        List<User> users = userDAO.getAllUsers();
+        List<Course> courses = courseDAO.getAllEnrolledCourses(users.get(0).getUserId());
+        List<Integer>  categories = gradeController.getCourseGradeCategories(courses.get(0).getCourseId() , users.get(0).getUserId());
+
+        assertEquals(gradeDAO.getGradeCategoriesForCourse(courses.get(0).getCourseId() , users.get(0).getUserId()), categories);
     }
 
     @Test
     public void getGradeByCategory() {
+        List<User> users = userDAO.getAllUsers();
+        List<Course> courses = courseDAO.getAllEnrolledCourses(users.get(0).getUserId());
+        List<Integer>  categories = gradeController.getCourseGradeCategories(courses.get(0).getCourseId() , users.get(0).getUserId());
+
+        String grade[]  = gradeController.getGradeByCategory(courses.get(0).getCourseId(), users.get(0).getUserId(), categories.get(0));
+
+        assertEquals("B", grade[0]);
+        assertEquals(83.334, Double.parseDouble(grade[1]), 0.01);
     }
 
     @Test
     public void getAssignmentGrade() {
+        List<Assignment> assignments = assignmentDAO.getAllAssignments();
+
+        String[] one = gradeController.getAssignmentGrade(assignments.get(0));
+        String[] two = gradeController.getAssignmentGrade(assignments.get(3));
+
+        assertEquals("B", one[0]);
+        assertEquals(83.334, Double.parseDouble(one[1]), 0.01);
+        assertEquals("F", two[0]);
+        assertEquals(50.000, Double.parseDouble(two[1]), 0.01);
     }
 
     @Test
-    public void getAssignmentByCategory() {
+    public void getAssignmentsByCategory() {
+        List<User> users = userDAO.getAllUsers();
+        List<Course> courses = courseDAO.getAllEnrolledCourses(users.get(0).getUserId());
+        List<Integer>  categories = gradeController.getCourseGradeCategories(courses.get(0).getCourseId() , users.get(0).getUserId());
+        List<Assignment> assignments = gradeController.getAssignmentsByCategory(courses.get(0).getCourseId() , users.get(0).getUserId(), categories.get(0).intValue());
+
+        assertEquals(assignmentDAO.getAssignmentsByCategory(courses.get(0).getCourseId() , users.get(0).getUserId(), categories.get(0).intValue()), assignments);
     }
 }
