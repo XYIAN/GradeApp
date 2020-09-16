65d2c4b2b27cd24b63e76ee2eccf25933349c2fb
Jason Buckley <jbuckley@csumb.edu>
2020-09-16 12:20:10 -0700

    Annotated the class

--- app/src/main/java/com/kxdilbeck/gradeapp/Controller/GradeController.java
+++ app/src/main/java/com/kxdilbeck/gradeapp/Controller/GradeController.java
@@ -10,13 +10,20 @@ import com.kxdilbeck.gradeapp.Model.Database.*;
 
 import java.util.List;
 
-
+/**
+ * Grade Controller provides grading utility methods for the view, or other controllers.
+ * It is able to calculate grades based on course, grade category, and assignment.
+ */
 public class GradeController {
     private UserDAO mUserDAO;
     private CourseDAO mCourseDAO;
     private AssignmentDAO mAssignmentDAO;
     private GradeDAO mGradeDAO;
 
+    /**
+     * Constructor for Grade Controller.
+     * @param context an application context
+     */
     public GradeController(Context context){
         AppDatabase db = Room.databaseBuilder(context, AppDatabase.class, AppDatabase.DBNAME)
                 .allowMainThreadQueries()
@@ -28,7 +35,10 @@ public class GradeController {
         mGradeDAO = db.getGradeDAO();
     }
 
-    // test/debug constructor
+    /**
+     *  Alternative constructor for Grade controller (Can be used in tests and debugging).
+     * @param db
+     */
     public GradeController(AppDatabase db){
         mUserDAO = db.getUserDAO();
         mCourseDAO = db.getCourseDAO();
@@ -36,6 +46,12 @@ public class GradeController {
         mGradeDAO = db.getGradeDAO();
     }
 
+    /**
+     * Gets the overall grade of a specific user for a course.
+     * @param userId
+     * @param courseId
+     * @return A String array with the first element being a letter grade, and the second a numeric grade.
+     */
     public String[] getCourseGrade(int userId, int courseId){
         List<Double> earnedPointsByCategory = mAssignmentDAO.getSumOfEarnedPointsByCourse(courseId, userId);
         List<Double> maxPointsByCategory = mAssignmentDAO.getSumOfMaxPointsByCourse(courseId, userId);
@@ -59,6 +75,11 @@ public class GradeController {
         return resultant;
     }
 
+    /**
+     * Given a numeric grade it gets the corresponding letter grade.
+     * @param grade
+     * @return returns a char holding the letter grade. ' ' means no grade.
+     */
     private char getLetterGrade(Double grade){
         if(grade >= 90 && grade != null && !grade.isNaN()){
             return 'A';
@@ -75,10 +96,23 @@ public class GradeController {
         return ' ';
     }
 
+    /**
+     * Gets all the grade categories for a course.
+     * @param courseId
+     * @param userId
+     * @return returns a list of grade category ids.
+     */
     public List<Integer> getCourseGradeCategories(int courseId, int userId){
         return mGradeDAO.getGradeCategoriesForCourse(courseId, userId);
     }
 
+    /**
+     * Gets the grade for a category in course for a specific user
+     * @param courseId
+     * @param userId
+     * @param categoryId
+     * @return A String array with the first element being a letter grade, and the second a numeric grade.
+     */
     public String[] getGradeByCategory(int courseId, int userId, int categoryId){
         List<Assignment> assignments = mAssignmentDAO.getAssignmentsByCategory(courseId, userId, categoryId);
         Double grade = 0.0;
@@ -97,6 +131,11 @@ public class GradeController {
         return resultant;
     }
 
+    /**
+     * Gets the grade of the assignment.
+     * @param assignment
+     * @return A String array with the first element being a letter grade, and the second a numeric grade.
+     */
     public String[] getAssignmentGrade(Assignment assignment){
         Double grade = 0.0;
 
@@ -110,8 +149,14 @@ public class GradeController {
         return resultant;
     }
 
+    /**
+     * Gets all the assignments in a category for a course for a specific user.
+     * @param courseId
+     * @param userId
+     * @param categoryId
+     * @return A list of assignments.
+     */
     public List<Assignment> getAssignmentsByCategory(int courseId, int userId, int categoryId){
         return mAssignmentDAO.getAssignmentsByCategory(courseId, userId, categoryId);
     }
-
 }
