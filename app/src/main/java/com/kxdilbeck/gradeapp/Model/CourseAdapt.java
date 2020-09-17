31cfd7b64e990220a55644f0fd3baa1419378da3
Jason Buckley <jbuckley@csumb.edu>
2020-09-17 15:12:13 -0700

    implemented adapter

--- app/src/main/java/com/kxdilbeck/gradeapp/Model/CourseAdapt.java
+++ app/src/main/java/com/kxdilbeck/gradeapp/Model/CourseAdapt.java
@@ -1,28 +1,42 @@
 package com.kxdilbeck.gradeapp.Model;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.kxdilbeck.gradeapp.R;
+
 import java.util.List;
 
 public class CourseAdapt extends RecyclerView.Adapter<CourseAdapt.CourseViewHolder>{
+    private List<Course> mCourses;
+    private List<String[]>  mGrades;
+    private View.OnClickListener mButtonAction;
 
-    private List<Course> listofCourses;
-    private List<Grade>  listofGrades;
+    public CourseAdapt(List<Course> courses, List<String[]> grades, View.OnClickListener buttonAction){
+        mCourses = courses;
+        mGrades = grades;
+        mButtonAction = buttonAction;
+    }
 
     @NonNull
     @Override
     public CourseAdapt.CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.courseitem, parent, false);
+        return new CourseViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CourseAdapt.CourseViewHolder holder, int position) {
+        Course course = mCourses.get(position);
+        String[] grade = mGrades.get(position);
 
+        holder.mActionButton.setText(course.getTitle() + "\t" + grade[0] + ": " + grade[1]);
+        holder.mActionButton.setTag(course);
     }
 
     @Override
@@ -31,8 +45,13 @@ public class CourseAdapt extends RecyclerView.Adapter<CourseAdapt.CourseViewHold
     }
 
     public class CourseViewHolder extends RecyclerView.ViewHolder {
-        public CourseViewHolder(@NonNull View itemView) {
+        public Button mActionButton;
+
+        public CourseViewHolder( View itemView) {
             super(itemView);
+            mActionButton = itemView.findViewById(R.id.courseRecyclerButton);
+            mActionButton.setOnClickListener(mButtonAction);
         }
+
     }
 }
